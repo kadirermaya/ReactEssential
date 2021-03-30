1f3599bb65692b5a875d291b052aab25dce96281
Kadir Ermaya <kadir.ermaya@gmail.com>
2021-03-29 23:31:05 -0400

    nested links with the react React Router 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
-import { Home, About, Events, Contact, Whoops404 } from "./pages"
+import { Home, About, Events, Contact, Whoops404, Services, CompanyHistory, Location } from "./pages"
 
 
 //https://api.github.com/users/kadirermaya
@@ -12,7 +12,11 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="/services" element={<Services />} />
+          <Route path="/history" element={<CompanyHistory />} />
+          <Route path="/location" element={<Location />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Whoops404 />} />
